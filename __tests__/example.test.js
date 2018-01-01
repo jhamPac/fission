9b1f6d2184f47459a6ef3b62d3c4aa0dea3ba666
Cloud <cloud@kevia.me>
2018-01-01 14:01:00 -0700

    removed example test and updated utils test

--- __tests__/example.test.js
+++
@@ -1,7 +0,0 @@
-let sum = (x, y) => x + y
-
-describe('Basic test examples', () => {
-  it('should add two numbers together', () => {
-      expect(sum(1, 2)).toEqual(3)
-  })
-})
---
+++ __tests__/utils.test.js
@@ -0,0 +1,7 @@
+import { createDirectoryContents } from '../utils'
+
+describe('Utility functions', () => {
+    test('createDirectoryContents should be a function', () => {
+      expect(typeof createDirectoryContents).toBe('function')
+    })
+})
